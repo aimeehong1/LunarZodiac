39e1211dd34889ef02d18b87f80d45a25ac8265d
Aimee Hong <honglp@bc.edu>
2024-09-16 19:08:43 -0400

    Lunar new year app

--- LunarZodiac/ContentView.swift
+++ LunarZodiac/ContentView.swift
@@ -8,12 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentYear = 2024
+    @State private var imageNumber = 4
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(verbatim: "\(currentYear)")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.red)
+            
+            Spacer()
+            
+            Image("image\(imageNumber)")
+                .resizable()
+                .scaledToFit()
+            
+            Spacer()
+            
+            HStack {
+                Button {
+                    imageNumber -= 1
+                    if imageNumber < 0 {
+                        imageNumber = 11
+                    }
+                    currentYear -= 1
+                } label: {
+                    Image(systemName: "chevron.left")
+                }
+                
+                Spacer()
+                
+                Button {
+                    imageNumber += 1
+                    if imageNumber > 11 {
+                        imageNumber = 0
+                    }
+                    currentYear += 1
+                } label: {
+                    Image(systemName: "chevron.right")
+                }
+            }
+            .padding()
+            .buttonStyle(.borderedProminent)
+            .tint(.red)
+            .font(.largeTitle)
+            .fontWeight(.black)
+            
         }
         .padding()
     }
